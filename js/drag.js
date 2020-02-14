f2db6911d6b1fb58413c6674e0d55df9b18f122b
kyle krylov <kylekrylov@MacBook-Pro-kyle.local>
2020-02-14 16:23:36 +0300

    add free ball

---
+++ js/drag.js
@@ -0,0 +1,27 @@
+"use strict";
+
+ball.onmousedown = function(event) {
+    let shiftX = event.clientX - ball.getBoundingClientRect().left;
+    let shiftY = event.clientY - ball.getBoundingClientRect().top;
+    ball.style.position = 'fixed';
+    ball.style.zIndex = 1000;
+    document.body.append(ball);
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+        ball.style.left = pageX - shiftX + 'px';
+        ball.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+    document.addEventListener('mousemove', onMouseMove);
+    ball.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        ball.onmouseup = null;
+    };
+};
+ball.ondragstart = function() {
+    return false;
+};
