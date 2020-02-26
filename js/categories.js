658a52c9dee9f71c4caa44d8af647099d0f9313f
kyle krylov <kylekrylov@MacBook-Pro-kyle.local>
2020-02-26 22:09:10 +0300

    v0.1.9

---
+++ js/categories.js
@@ -0,0 +1,15 @@
+"use strict";
+//
+$(function() {
+    $('.portfolio_element_galery_menu button').click(function() {
+        var get_id = this.id;
+        var get_current = $('.portfolio_element_galery_projects_project.' + get_id);
+
+        $('.portfolio_element_galery_projects_project').not(get_current).hide(100);
+        get_current.show(100);
+    });
+
+    $('#all').click(function() {
+        $('.portfolio_element_galery_projects_project').show(100);
+    });
+});
